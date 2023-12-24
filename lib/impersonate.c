dc27e469d91c20554cc555e48bad67c759657cc6
Yifei Kong <kong@yifei.me>
2023-12-24 23:09:30 +0800

    Fix chrome119

--- lib/impersonate.c
+++ lib/impersonate.c
@@ -319,7 +319,7 @@ const struct impersonate_opts impersonations[] = {
       .http_headers = {
               "sec-ch-ua: \"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
               "sec-ch-ua-mobile: ?0",
-              "sec-ch-ua-platform: \"Windows\"",
+              "sec-ch-ua-platform: \"macOS\"",
               "Upgrade-Insecure-Requests: 1",
               "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
               "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
